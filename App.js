591470c55f81188e0395b57e9ceabe1767efe1a8
slindebjerg <proj.alph@gmail.com>
2019-02-13 17:33:10 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          onPress={this.handleClick}
+          title="Press me!"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+class foo extends Button{
+  handleClick() {
+    console.log("Button clicked");
+  }
+}
